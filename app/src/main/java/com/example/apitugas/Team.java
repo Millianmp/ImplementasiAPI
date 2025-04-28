6f6988bed55811a1d6f1e50602bca1bcebe30839
DraniXx-PNG <rajendraghasia28@gmail.com>
2025-04-28 12:48:39 +0700

    initial commit

---
+++ app/src/main/java/com/example/apitugas/Team.java
@@ -0,0 +1,18 @@
+package com.example.apitugas;
+
+import com.google.gson.annotations.SerializedName;
+
+public  class Team {
+
+    @SerializedName("strTeam")
+    public String strTeam;
+
+    @SerializedName("strStadium")
+    public String strStadium;
+
+    @SerializedName("strBadge")
+    public String strBadge;
+
+
+
+}
